6b89dd8d32f68d0caf90f587ffeb76c9a427e224
teyim <teyimasobo@gmail.com>
2024-12-30 15:41:56 +0100

    modify scraper

---
+++ lib/scraper.ts
@@ -0,0 +1,48 @@
+import { JobPost } from "../types";
+import * as chrono from "chrono-node";
+import {
+  initializeBrowser,
+  removeDuplicates,
+  scrapeJobsForTerm,
+  sortJobsByPostedDateDescending,
+} from "../utils";
+
+import { closeDatabase, getCollection, upsertJobs } from "../dbHelpers";
+import { DB_COLLECTION } from "../constants";
+
+// Main scraping function
+export async function scrapeData() {
+  const searchTerms = [
+    "javascript",
+    "developer",
+    "frontend",
+    "frontend developer",
+  ];
+  const jobs: JobPost[] = [];
+  const browser = await initializeBrowser();
+
+  try {
+    for (const term of searchTerms) {
+      const termJobs = await scrapeJobsForTerm(browser, term);
+      jobs.push(...termJobs);
+    }
+
+    const jobsWithDates = jobs.map((job) => ({
+      ...job,
+      postedDate: chrono.parseDate(job.posted) as any,
+    }));
+
+    const uniqueJobs = removeDuplicates(jobsWithDates);
+
+    const sortedUniqueJobs = sortJobsByPostedDateDescending(uniqueJobs);
+    console.log(sortedUniqueJobs);
+
+    const collection = await getCollection(DB_COLLECTION);
+
+    await upsertJobs(sortedUniqueJobs, collection);
+  } catch (error) {
+    console.error("Error during scraping process:", error);
+  } finally {
+    await browser.close();
+  }
+}
