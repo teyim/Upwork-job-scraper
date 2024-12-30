d64df3aa06d0be58d72d67627894edb9c1745c0c
TEYIM ASOBO <48864977+teyim@users.noreply.github.com>
2024-12-30 22:24:19 +0100

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -6,7 +6,16 @@ export async function initializeBrowser() {
   try {
     const { browser } = await connect({
       headless: true,
-      args: ["--start-maximized"],
+      args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
       customConfig: {},
       turnstile: true,
       connectOption: { defaultViewport: null },
