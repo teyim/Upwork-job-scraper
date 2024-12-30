c1582317044b7ab5e6977d035d0ffbf1dc6d4330
teyim <teyimasobo@gmail.com>
2024-12-30 15:58:27 +0100

    fix chat undefined error

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,7 @@ export async function handler(req: Request) {
   const { body } = req;
 
   if (body) {
-    const chatId = body.message.chat.id;
+    const chatId = body?.message?.chat?.id;
     const messageText = body.message.text;
 
     console.log(chatId);
